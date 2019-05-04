fe236d2418974004eca18fca52afcb5332179daf
Alessandro Freitas <alessandro.spiercort@hotmail.com>
2019-05-04 15:43:33 -0300

    fix issues eslint

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://react-node-app-backend.herokuapp.com'
-})
+  baseURL: 'https://react-node-app-backend.herokuapp.com',
+});
 
 export default api;
